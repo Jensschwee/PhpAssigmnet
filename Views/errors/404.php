e316a7b8a2211dbc66ed6e4ad070905280146e98
Jens Hjort Schwee <jensschwee@msn.com>
2016-04-02 12:19:04 +0200

    error page

--- Views/errors/404.php
+++ Views/errors/404.php
@@ -1,6 +1,14 @@
 <?php require VIEW_DIR . '/header.php'; ?>
-
-<p>404 File Not Found</p>
-<p><a href="/">Go to the frontpage</a></p>
-
+<div id="menu_outer">
+    <div class="menu_inner">
+        <ul>
+            <li><a href="Gallery">Gallery</a></li>
+            <li><a href="Upload">Upload</a></li>
+            <li><a href="ShowUsers" >Userlist</a></li>
+            <li><a href="AddUser">Add user</a></li>
+            <li><a href="Logout">Logout</a></li>
+        </ul>
+    </div>
+</div>
+<h1>HTTP Error 404</h1>
 <?php require VIEW_DIR . '/footer.php'; ?>
